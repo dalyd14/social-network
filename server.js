5a3c8720dc882e8c6891b609c848b62f19f587ae
dalyd14 <dalyd14@gmail.com>
2021-03-05 15:06:25 -0500

    created framework; installed dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect('mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true)
+
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
