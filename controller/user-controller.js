f8b422b1095a1fe313125c8ba4862ea04389a428
dalyd14 <dalyd14@gmail.com>
2021-03-05 15:55:37 -0500

    created user controller... atleast the beginning of it

---
+++ controller/user-controller.js
@@ -0,0 +1,49 @@
+const { User } = require('../model')
+
+const userController = {
+    getAllUsers (req, res) {
+        User.find()
+        .then(dbUser => {
+            res.json(dbUser)
+        })
+        .catch(err => res.status(500).json(err))
+    },
+
+    getUser ({ params }, res) {
+        User.findOne({ _id: params.userId })
+        .then(dbUser => {
+            res.json(dbUser)
+        })
+        .catch(err => res.status(500).json(err))
+    },
+
+    createUser ({ body }, res) {
+        User.create(body)
+        .then(dbUser => {
+            res.json(dbUser)
+        })
+        .catch(err => res.status(500).json(err))
+    },
+
+    updateUser ({ params, body }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            body,
+            { new: true }
+        )
+        .then(dbUser => {
+            res.json(dbUser)
+        })
+        .catch(err => res.status(500).json(err))
+    },
+
+    deleteUser ({ params }, res) {
+        User.findOneAndDelete({ _id: params.userId })
+        .then(dbUser => {
+            res.json(dbUser)
+        })
+        .catch(err => res.status(500).json(err))
+    },
+}
+
+module.exports = userController
