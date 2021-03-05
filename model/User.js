72d39887ee6756ffca3546c4bcf5aed0205774ca
dalyd14 <dalyd14@gmail.com>
2021-03-05 15:26:27 -0500

    finished user model

---
+++ model/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'You must provide a username',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
