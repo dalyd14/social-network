813fd388554f87f3d066c936b4b106e7be47d9fa
dalyd14 <dalyd14@gmail.com>
2021-03-05 15:44:09 -0500

    finished thought model along with reply schema

--- model/User.js
+++ model/User.js
@@ -34,3 +34,7 @@ const userSchema = new Schema(
 userSchema.virtual('friendCount').get(function() {
     return this.friends.length
 })
+
+const User = model('User', userSchema)
+
+module.exports = User
---
+++ utils/dateFormatter.js
@@ -0,0 +1,7 @@
+const moment = require('moment')
+
+const dateformatter = (timeStamp) => {
+    console.log(timeStamp)
+}
+
+module.exports = dateformatter
