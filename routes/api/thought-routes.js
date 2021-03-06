9e2f3655ebed6f30c5bfe91cd16259e875a484fd
dalyd14 <dalyd14@gmail.com>
2021-03-06 07:27:09 -0500

    created all routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+
+const {
+    getAllThoughts,
+    getThought,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controller/thought-controller')
+const {
+    createReaction,
+    deleteReaction
+} = require('../../controller/reaction-controller')
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought)
+
+router
+    .route('/:thoughtId')
+    .get(getThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction)
---
+++ routes/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+
+const {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controller/user-controller')
+const {
+    addFriendToFriendsList,
+    deleteFriendFromFriendsList
+} = require('../../controller/friend-controller')
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser)
+
+router
+    .route('/:userId')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriendToFriendsList)
+    .delete(deleteFriendFromFriendsList)
